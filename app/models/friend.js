f36f83f0e70a525a73bef0b8f921e53ad362aa44
paavani <pavanisd9@gmail.com>
2015-08-11 20:30:37 +0530

    add attributes

--- app/models/friend.js
+++ app/models/friend.js
@@ -1,6 +1,10 @@
-import DS from 'ember-data';
+'import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+ firstName: DS.attr('string'),
+ lastName: DS.attr('string'),
+ email: DS.attr('string'),
+ twitter: DS.attr('string'),
+ totalArticals: DS.attr('number')
 
 });
